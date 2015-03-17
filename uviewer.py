099419a227a2e91c6a383ca43e41a4a4da0db206
Martin Casado <martin.casado@gmail.com>
2015-03-16 20:49:30 -0700

    Add a viewer class to track position and direction
    Create a seperate viewer abstraction which will eventually allow us to
    rotate the viewer independently of the rotate of each galaxy

---
+++ uviewer.py
@@ -0,0 +1,15 @@
+"""
+
+Viewer of universe
+
+Track position, field of view, and direction of viewer
+  
+"""
+
+from Vec3d import Vec3d
+
+class uviewer:
+
+    def __init__(self, position = Vec3d(0,0,0), direction = Vec3d(0,0,1)):
+        self.position  = position
+        self.direction = direction
